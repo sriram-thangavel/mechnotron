c17b0dc06bc2d9ef5fdafcc2e3225b3ef250cb1b
Sriram Thangavel R <83130083+sriram-thangavel@users.noreply.github.com>
2023-09-23 20:17:10 +0530

    Update settings.py

--- mechnotron/settings.py
+++ mechnotron/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': "mechnotron", 
         'USER': "postgres", 
-        'PASSWORD': "9787565407Ss#",
+        'PASSWORD': "",
         'HOST': "127.0.0.1", 
         'PORT': "5432",
     }
